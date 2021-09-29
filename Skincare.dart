0cc37439135276e9abd1d0879d2c60f1a6a026cd
adindaputrinr <84336870+adindaputrinr@users.noreply.github.com>
2021-09-30 01:35:51 +0700

    first commit

--- Skincare.dart
+++ Skincare.dart
@@ -1,1 +1,20 @@
-"# dart-oop" 
+void main() {
+  Skincare s = new Skincare();
+  s.deskripsi();
+}
+
+class Skincare {
+  String jenis = "Serum";
+  String brand = "Avoskin";
+  String varian = "Alpha Arbutin";
+  int harga = 139000;
+
+  void deskripsi() {
+    print(jenis);
+    print(brand);
+    print(varian);
+    print(harga);
+  }
+}
+# dart-oop
+# dart-oop
